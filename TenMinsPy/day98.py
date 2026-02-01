bf6d31080e62dc129670a50eac64ca3f2922766b
maru <217520849+maru395@users.noreply.github.com>
2026-02-01 22:44:42 +0800

    update

---
+++ TenMinsPy/day98.py
@@ -0,0 +1,20 @@
+# addColumn(columnName) a function that will add a new column to the csv
+
+import csv
+
+def addColumn(columnName, file_path="D:/coding30minsAday/textFiles/JTrialCsv.csv"):
+    try:
+        # Step 1: Read all lines
+        with open(file_path, "r") as f:
+            lines = f.readlines()
+            
+        lines[0] = lines[0].strip() + "," + columnName + "\n"
+        for i in range(1, len(lines)):
+            lines[i] = lines[i].strip() + ",\n"
+
+        # Step 3: Rewrite the CSV
+        with open(file_path, "w") as f:
+            f.writelines(lines)
+
+    except Exception as e:
+        print("Error:", e)
